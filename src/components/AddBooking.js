65e7fae0898f06ffe5936ab079d71bce75056944
Sajin-storm <Aistator@gmail.com>
2021-04-08 09:19:22 +0530

    grid layout for add booking

--- src/components/AddBooking.js
+++ src/components/AddBooking.js
@@ -5,7 +5,9 @@ import * as actions from '../actions/action'
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,6 +16,11 @@ const useStyles = makeStyles((theme) => ({
         width: '25ch',
       },
     },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
   }));
 
 
@@ -76,63 +83,218 @@ class AddBooking extends Component {
    
         return (
 
-            
+            <div >
+
+<div style={{display:'flex', justifyContent:'center'}}>&emsp;<h3>Add New Booking</h3></div>
+
+<Container component="main" maxWidth="xs">
+      
+      <Grid container spacing={4}>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.bookingId} 
+          id="outlined-required" 
+          label="Enter Booking ID" 
+          variant="filled" 
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+              <TextField 
+              fullWidth
+              inputRef={this.username} 
+              id="outlined-required" 
+              label="Enter Username" 
+              variant="filled" 
+              ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.busNumber} 
+          id="outlined-required" 
+          label="Enter Bus Number" 
+          variant="filled" 
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.source} 
+          id="outlined-required" 
+          label="Enter Source" 
+          variant="filled" 
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.destination} 
+          id="outlined-required" 
+          label="Enter Destination" 
+          variant="filled" 
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.numberOfSeats} 
+          id="outlined-required" 
+          label="Enter number of seats" 
+          variant="filled" 
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField 
+          fullWidth
+          inputRef={this.amountPaid} 
+          id="outlined-required" 
+          label="Enter Amount" 
+          variant="filled" 
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.date} 
+          id="outlined-required" 
+          label="Enter date" 
+          type='date' 
+          variant="filled" 
+          InputLabelProps={{shrink: true}}
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.journeyStartTime} 
+          id="outlined-required" 
+          type='time' 
+          label="Enter start time" 
+          variant="filled" 
+          InputLabelProps={{shrink: true}}
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+          <TextField  
+          fullWidth
+          inputRef={this.journeyEndTime} 
+          id="outlined-required" 
+          type='time' 
+          label="Enter end time" 
+          variant="filled" 
+          InputLabelProps={{shrink: true}}
+          ></TextField>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+        
+        
+        <Button
+            // type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            //className={classes.submit}
+            onClick={this.addBooking.bind(this)}
+           
+          >
+              
+            Add Booking
+          </Button>
+      </Grid>
+
+      </Grid>
+      
+      </Container>
+
+    </div>
 
 
 
-            <div style={{
-                display:'flex',
-                justifyContent:'center',
-            }}>
+            // <div style={{
+            //     display:'flex',
+            //     justifyContent:'center',
+            // }}>
                 
                
                
-                 <form className={classes.root} noValidate autoComplete="off" >
+            //      <form className={classes.root} noValidate autoComplete="off" >
                  
-                <div style={{display:'flex', justifyContent:'center'}}>&emsp;<h3>Add New Booking</h3></div>
-                &emsp;
+            //     <div style={{display:'flex', justifyContent:'center'}}>&emsp;<h3>Add New Booking</h3></div>
+            //     &emsp;
                  
-                 <TextField  inputRef={this.bookingId} id="outlined-required" label="Enter Booking ID" variant="filled" ></TextField>
-                 &emsp;
+            //      <TextField  inputRef={this.bookingId} id="outlined-required" label="Enter Booking ID" variant="filled" ></TextField>
+            //      &emsp;
 
-                 <TextField  inputRef={this.username} id="outlined-required" label="Enter Username" variant="filled" ></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField  inputRef={this.username} id="outlined-required" label="Enter Username" variant="filled" ></TextField>
+            //      <br/><br/>&emsp;
 
-                 <TextField  inputRef={this.busNumber} id="outlined-required" label="Enter Bus Number" variant="filled" ></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField  inputRef={this.busNumber} id="outlined-required" label="Enter Bus Number" variant="filled" ></TextField>
+            //      <br/><br/>&emsp;
 
-                 <TextField  inputRef={this.source} id="outlined-required" label="Enter Source" variant="filled" ></TextField>
-                 &emsp;
+            //      <TextField  inputRef={this.source} id="outlined-required" label="Enter Source" variant="filled" ></TextField>
+            //      &emsp;
 
-                 <TextField  inputRef={this.destination} id="outlined-required" label="Enter Destination" variant="filled" ></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField  inputRef={this.destination} id="outlined-required" label="Enter Destination" variant="filled" ></TextField>
+            //      <br/><br/>&emsp;
 
-                 <TextField  inputRef={this.numberOfSeats} id="outlined-required" label="Enter number of seats" variant="filled" ></TextField>
-                 &emsp;
+            //      <TextField  inputRef={this.numberOfSeats} id="outlined-required" label="Enter number of seats" variant="filled" ></TextField>
+            //      &emsp;
 
-                 <TextField inputRef={this.amountPaid} id="outlined-required" label="Enter Amount" variant="filled" ></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField inputRef={this.amountPaid} id="outlined-required" label="Enter Amount" variant="filled" ></TextField>
+            //      <br/><br/>&emsp;
 
-                 <TextField  inputRef={this.date} id="outlined-required" label="Enter date" type='date' variant="filled" InputLabelProps={{shrink: true}}></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField  inputRef={this.date} id="outlined-required" label="Enter date" type='date' variant="filled" InputLabelProps={{shrink: true}}></TextField>
+            //      <br/><br/>&emsp;
 
-                 <TextField  inputRef={this.journeyStartTime} id="outlined-required" type='time' label="Enter start time" variant="filled" InputLabelProps={{shrink: true}}></TextField>
-                 &emsp;
+            //      <TextField  inputRef={this.journeyStartTime} id="outlined-required" type='time' label="Enter start time" variant="filled" InputLabelProps={{shrink: true}}></TextField>
+            //      &emsp;
 
-                 <TextField  inputRef={this.journeyEndTime} id="outlined-required" type='time' label="Enter end time" variant="filled" InputLabelProps={{shrink: true}}></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField  inputRef={this.journeyEndTime} id="outlined-required" type='time' label="Enter end time" variant="filled" InputLabelProps={{shrink: true}}></TextField>
+            //      <br/><br/>&emsp;
 
-                 <Button variant="contained" color="primary" className={classes.button} onClick={this.addBooking.bind(this)}>Add Booking</Button> 
-                 <br/><br/>&emsp;
+            //      <Button variant="contained" color="primary" className={classes.button} onClick={this.addBooking.bind(this)}>Add Booking</Button> 
+            //      <br/><br/>&emsp;
 
-                 <TextField fullWidth  disabled id="outlined-required" label={this.props.message} variant="standard" InputProps={{readOnly: true,}}></TextField>
-                 <br/><br/>&emsp;
+            //      <TextField fullWidth  disabled id="outlined-required" label={this.props.message} variant="standard" InputProps={{readOnly: true,}}></TextField>
+            //      <br/><br/>&emsp;
 
-                 <TextField fullWidth  disabled id="outlined-required"  variant="standard" InputProps={{readOnly: true,}}></TextField>
+            //      <TextField fullWidth  disabled id="outlined-required"  variant="standard" InputProps={{readOnly: true,}}></TextField>
                  
-                 </form>
+            //      </form>
 
-            </div>
+            // </div>
             
         )
     }
